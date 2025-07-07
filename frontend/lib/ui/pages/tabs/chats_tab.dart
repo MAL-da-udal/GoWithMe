30397e6c9e817e6b47558a49a1f63cf908aedf14
mc_lavrushka <mc.lavrushka@gmail.com>
2025-07-08 02:25:19 +0300

    feat: add go_router with routing file and blank pages

---
+++ frontend/lib/ui/pages/tabs/chats_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatsTab extends StatefulWidget {
+  const ChatsTab({super.key});
+
+  @override
+  State<ChatsTab> createState() => _ChatsTabState();
+}
+
+class _ChatsTabState extends State<ChatsTab> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
