6051d91dcc695f7fc1a98667e5162c4bddd3f054
mc_lavrushka <mc.lavrushka@gmail.com>
2025-07-10 00:38:31 +0300

    feat: init version of home page with tabs and implemented profile

--- frontend/lib/ui/pages/bottom_tabs/profile_tab.dart
+++ frontend/lib/ui/pages/bottom_tabs/profile_tab.dart
@@ -8,8 +8,115 @@ class ProfileTab extends StatefulWidget {
 }
 
 class _ProfileTabState extends State<ProfileTab> {
+  String name = '';
+  String gender = 'Ж';
+  int age = 0;
+  String description = '';
+  Set<String> selectedActivities = {};
+  final nameController = TextEditingController();
+  final surnameController = TextEditingController();
+  final ageController = TextEditingController();
+  final descriptionController = TextEditingController();
+
+  final activities = [
+    'Йога',
+    'Бег',
+    'Велосипед',
+    'Плавание',
+    'Футбол',
+    'Танцы',
+    'Питание',
+    'Медитация',
+    'Походы',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 50,
+            child: Text(name == '' ? "" : name[0].toUpperCase()),
+          ),
+          const SizedBox(height: 16),
+
+          TextField(
+            controller: nameController,
+            decoration: const InputDecoration(labelText: 'Имя'),
+          ),
+
+          TextField(
+            controller: surnameController,
+            decoration: const InputDecoration(labelText: 'Фамилия'),
+          ),
+
+          TextField(
+            controller: ageController,
+            decoration: const InputDecoration(labelText: 'Возраст'),
+            keyboardType: TextInputType.number,
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ChoiceChip(
+                label: const Text('Ж'),
+                selected: gender == 'Ж',
+                onSelected: (_) => setState(() => gender = 'Ж'),
+              ),
+              const SizedBox(width: 8),
+              ChoiceChip(
+                label: const Text('М'),
+                selected: gender == 'М',
+                onSelected: (_) => setState(() => gender = 'М'),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 16),
+
+          TextField(
+            controller: descriptionController,
+            decoration: const InputDecoration(
+              labelText: 'Описание',
+              border: OutlineInputBorder(),
+            ),
+            maxLines: 3,
+          ),
+
+          const SizedBox(height: 16),
+
+          Wrap(
+            spacing: 8,
+            runSpacing: 8,
+            children: activities.map((act) {
+              final isSelected = selectedActivities.contains(act);
+              return FilterChip(
+                label: Text(act),
+                selected: isSelected,
+                onSelected: (_) {
+                  setState(() {
+                    isSelected
+                        ? selectedActivities.remove(act)
+                        : selectedActivities.add(act);
+                  });
+                },
+              );
+            }).toList(),
+          ),
+
+          const SizedBox(height: 20),
+
+          ElevatedButton(
+            onPressed: () {
+              //TODO: add implementation
+            },
+            child: const Text("Сохранить"),
+          ),
+        ],
+      ),
+    );
   }
 }
--- frontend/lib/ui/pages/home_page.dart
+++ frontend/lib/ui/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_with_me/ui/pages/bottom_tabs/profile_tab.dart';
+import 'package:go_with_me/ui/pages/bottom_tabs/search_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,10 +10,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [SearchTab(), ProfileTab()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Home page"),
+      body: IndexedStack(index: _currentIndex, children: _pages),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (newIndex) => setState(() => _currentIndex = newIndex),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Поиск'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Профиль'),
+        ],
+      ),
     );
   }
 }
