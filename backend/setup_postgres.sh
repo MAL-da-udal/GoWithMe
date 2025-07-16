fd99d3ce4087216856e42070fa6d7458a79cc7f2
Alexander Blinov <mhdtadev@gmail.com>
2025-07-16 16:05:18 +0300

    fix: Checking for user exists in setup postgres script

--- backend/setup_postgres.sh
+++ backend/setup_postgres.sh
@@ -10,6 +10,6 @@ until su - postgres -c "psql -c '\l'"; do
 done
 
 
-su - postgres -c "psql -c \"CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';\""
-su - postgres -c "createdb $POSTGRES_DB"
+su - postgres -c "psql -c \"DO \$\$ BEGIN IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$POSTGRES_USER') THEN CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD'; END IF; END \$\$;\""
+su - postgres -c "psql -c \"SELECT 1 FROM pg_database WHERE datname = '$POSTGRES_DB'\" | grep -q 1 || createdb $POSTGRES_DB"
 su - postgres -c "psql -c 'GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;'"
